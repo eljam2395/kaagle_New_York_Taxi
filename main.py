a9579fff608c45cc823631642eef1bdc7df6a48d
Eljam <boucheemaxime@gmail.com>
2020-05-07 15:49:29 +0200

    add the calculation of the number of km per day

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from math import *
 def haversine_dist(lon1, lat1, lon2, lat2):
     """
     Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
+    on earth
     """
     # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
@@ -15,61 +15,73 @@ def haversine_dist(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371
     return c * r
 
 
 def mean_speed(df):
     """ Caculate the mean speed on each drive.
         in param:
-            dataframe that contains the id the trip duration and the pickup/dropoff coordinates
+            dataframe that contains the id, the trip duration and the pickup/dropoff coordinates
         out param:
             dataframe that contains the id and the mean speed of the drive"""
     udf_distance = udf(haversine_dist)
-    distance = df.select(df.id, df.trip_duration.cast('float'), df.pickup_longitude.cast('float'),
-                         df.pickup_latitude.cast('float'), df.dropoff_longitude.cast('float'),
-                         df.dropoff_latitude.cast('float'))
 
-    distance = distance.withColumn('trip_duration', distance.trip_duration / 3600.0)
-    distance = distance.withColumn('dist', udf_distance(distance.pickup_longitude, distance.pickup_latitude,
-                                                        distance.dropoff_longitude, distance.dropoff_latitude))
+    # conversion to hour
+    distance = df.withColumn('trip_duration', df.trip_duration.cast('float') / 3600.0)
+
+    distance = distance.withColumn('dist', udf_distance(df.pickup_longitude.cast('float'),
+                                                        df.pickup_latitude.cast('float'),
+                                                        df.dropoff_longitude.cast('float'),
+                                                        df.dropoff_latitude.cast('float')).cast('float'))
 
     df_mean_speed = distance.withColumn('meanSpeed(km/h)', distance.dist / distance.trip_duration)
 
-    columns_to_drop = ["pickup_longitude", "pickup_latitude", "dropoff_longitude", "dropoff_latitude", "dist",
-                       "trip_duration"]
-    df_mean_speed = df_mean_speed.drop(*columns_to_drop)
-
-    return df_mean_speed
+    return df_mean_speed.select("id", "meanSpeed(km/h)")
 
 
 def get_nb_drive_per_day(df):
-    """ calculate the number of drive per day of the week.
+    """ calculate the number of drives per day of week.
     in param:
             dataframe that contains the pickup_datetime
         out param:
-            dataframe that contains the day of the week and the number of courses """
-    day = df.select("pickup_datetime")
-    day = day.withColumn('pickup_datetime', date_format(day.pickup_datetime, "E"))
+            dataframe that contains the day of week and the number of drives """
+
+    day = df.withColumn('pickup_datetime', date_format(df.pickup_datetime, "E"))
     return day.groupBy('pickup_datetime').count()
 
 
 def get_nb_drive_per_slice_day(df):
-    """ calculate the number of drive per slice of day.
+    """ calculate the number of drives per slice of day. A slice of day corresponds to six hours.
     in param:
             dataframe that contains the pickup_datetime
         out param:
-            dictionnary that contains the drive per slice of day """
+            dictionnary that contains the drives per slice of day """
 
-    hour = df.select("pickup_datetime")
-    hour = hour.withColumn('pickup_datetime', date_format(hour.pickup_datetime, "H"))
-    # 0-6 6-12 12-18 18-24
+    hour = df.withColumn('pickup_datetime', date_format(df.pickup_datetime, "H"))
 
     zero_to_six = hour.filter(hour.pickup_datetime.between(0, 6)).count()
     six_to_twelve = hour.filter(hour.pickup_datetime.between(7, 12)).count()
     twelve_to_eighteen = hour.filter(hour.pickup_datetime.between(13, 18)).count()
     eighteen_to_twenty_for = hour.filter(hour.pickup_datetime.between(19, 26)).count()
 
-    result = {"0-6": zero_to_six, "6-12": six_to_twelve, "12-18": twelve_to_eighteen, "18-24": eighteen_to_twenty_for}
+    return {"0-6": zero_to_six, "6-12": six_to_twelve, "12-18": twelve_to_eighteen, "18-24": eighteen_to_twenty_for}
 
-    return result
+
+def get_km_per_day(df):
+    """ calculate the number of km done per day.
+    in param:
+            dataframe that contains the pickup_datetime and pickup/dropoff coordinates
+        out param:
+            dataframe that contains the km per day """
+
+    udf_distance = udf(haversine_dist)
+
+    km_by_day = df.withColumn("dist", udf_distance(df.pickup_longitude.cast('float'),
+                                                   df.pickup_latitude.cast('float'),
+                                                   df.dropoff_longitude.cast('float'),
+                                                   df.dropoff_latitude.cast('float')).cast('float'))
+
+    km_by_day = km_by_day.withColumn('pickup_datetime', date_format(km_by_day.pickup_datetime, "E"))
+    return km_by_day.groupBy('pickup_datetime').sum()
--- test_main.py
+++ test_main.py
@@ -6,26 +6,40 @@ from pyspark.sql import SparkSession
 class TestMain(unittest.TestCase):
 
     def setUp(self):
-        """Initialisation des tests."""
+        """Init of tests """
         spark = SparkSession.builder.appName('Taxi').getOrCreate()
         self.taxiDf = spark.read.csv("resources/train-10000.csv", header=True)
 
     def test_meanSpeed(self):
-        """ verify the result of the meanSpeed function"""
+        """ verify the result of meanSpeed function for the first 5 values"""
         df_mean_speed = main.mean_speed(self.taxiDf)
         df_mean_speed.show()
         list_mean_speed = [row["meanSpeed(km/h)"] for row in df_mean_speed.take(5)]
-        self.assertEqual(list_mean_speed, [11.856428146656878, 9.803658835090804, 10.82220083941101, 12.465721030245636, 9.836594146211462])
+        compare = [11.856428146656878, 9.803658835090804, 10.82220083941101, 12.465721030245636, 9.836594146211462]
+        for i in range(len(list_mean_speed)):
+            self.assertAlmostEqual(list_mean_speed[i], compare[i], places=4)
 
     def test_get_nb_drive_per_day(self):
-        driver_per_day = main.get_nb_drive_per_day(self.taxiDf)
-        driver_per_day.show()
-        list_driver_per_day = [row["count"] for row in driver_per_day.collect()]
+        """ verify the result of get_nb_drive_per_day function"""
+        drive_per_day = main.get_nb_drive_per_day(self.taxiDf)
+        drive_per_day.show()
+        list_driver_per_day = [row["count"] for row in drive_per_day.collect()]
         self.assertEqual(list_driver_per_day, [1356, 1286, 1475, 1514, 1428, 1420, 1520])
 
     def test_get_nb_drive_per_slice_day(self):
+        """ verify the result of get_nb_drive_per_slice_day function"""
         nb_drive_per_slice_day = main.get_nb_drive_per_slice_day(self.taxiDf)
         self.assertEqual(nb_drive_per_slice_day["0-6"], 1422)
         self.assertEqual(nb_drive_per_slice_day["6-12"], 2643)
         self.assertEqual(nb_drive_per_slice_day["12-18"], 3092)
         self.assertEqual(nb_drive_per_slice_day["18-24"], 2842)
+
+    def test_get_km_per_day(self):
+        """ verify the result of get_km_per_day function"""
+        nb_km_by_day = main.get_km_per_day(self.taxiDf)
+        nb_km_by_day.show()
+        list_km_by_day = [row["sum(dist)"] for row in nb_km_by_day.collect()]
+        compare = [4789.794910823723, 4530.8014779966325, 4994.494979438139, 4932.921239493415,
+                   5028.187267415691, 4704.324866755778, 5073.513208014454]
+        for i in range(len(list_km_by_day)):
+            self.assertAlmostEqual(list_km_by_day[i], compare[i], places=4)
